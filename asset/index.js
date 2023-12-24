83cc3b18732e864751d3fe8153d3b7f591475034
Naksuuuu <ariel.233040088@mail.unpas.ac.id>
2023-12-24 12:55:41 +0700

    project 1 portfolio

--- asset/index.js
+++ asset/index.js
@@ -13,7 +13,8 @@ animated();
 
 setInterval(animated, 8000);
 
-// form
+// form // form to google sheets
+
 const formItems = Array.from(document.querySelectorAll('.form-items input'));
 const formTextArea = document.querySelector('.form-items textarea');
 const formButton = document.querySelector('.form-items button');
@@ -24,17 +25,45 @@ formItems.forEach((input) => {
 
 formTextArea.addEventListener('input', processInput);
 
-function processInput() {
-  const allInput = formItems.every((item) => item.value != '');
-  if (allInput && formTextArea.value.trim() != '') {
-    formButton.style.backgroundColor = 'royalBlue';
-    formButton.addEventListener('click', (btn) => {
-      btn.preventDefault();
-      formItems.forEach((input) => {
-        input.value = '';
-      });
-      formTextArea.value = '';
-      formButton.style.backgroundColor = '';
-    });
+formButton.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const allInput = formItems.every((item) => item.value.trim() !== '');
+  if (allInput && formTextArea.value.trim() !== '') {
+    submitForm();
   }
+});
+
+function processInput() {
+  const allInput = formItems.every((item) => item.value.trim() !== '');
+  if (allInput && formTextArea.value.trim() !== '') {
+    formButton.style.backgroundColor = 'royalBlue';
+  } else {
+    formButton.style.backgroundColor = '';
+  }
+}
+
+function submitForm() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyWkkAGymQshH3qvS9kFvWMmoMp_jTSuxZ4k7kPBMF9sYrr2LKYVyUAjVhNyHxC8zYv/exec';
+  const form = document.forms['portfolio-contact-form'];
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      console.log('Success!', response);
+      alert('pesan terkirim!');
+    })
+    .catch((error) => {
+      console.error('Error!', error.message);
+      alert('pesan tidak terkirim(error)!');
+    });
+
+  formItems.forEach((input) => {
+    input.value = '';
+  });
+  formTextArea.value = '';
+  formButton.style.backgroundColor = '';
 }
